09ebfca71da2ff14a4bee844a6a59bc3eeee9549
Richard Mills <scripts.richard@gmail.com>
2018-04-14 17:23:54 -0600

    Added comments.

--- reading_time.js
+++ reading_time.js
@@ -1,15 +1,22 @@
+// Constants
 // Estimated reading speed of 200 words per minute.
 const reading_speed = 200;
 const background_color = "black";
 const font_color = "white";
 
 
+/*
+ * Given a string, returns the word-count.
+ */
 function count_words(str) {
   let matches = str.match(/[\w\d]+/gi);
   return matches ? matches.length : 0;
 }
 
 
+/*
+ * Removes the Reading Time element from the page's body.
+ */
 function remove_element() {
   console.log("function called");
   let element = document.getElementById("reading_time_element");
@@ -17,6 +24,10 @@ function remove_element() {
 }
 
 
+/*
+ * Adds the reading time element to the page's body with the number of minutes.
+ * Styling for the element is added here.
+ */
 function create_element(minutes) {
   let element = document.createElement("div");
   element.style.zIndex = "99999";
@@ -50,6 +61,10 @@ function create_element(minutes) {
 }
 
 
+/*
+ * Only count words and create the element once given the window.onload.
+ * Depending on page, can take awhile for window.onload to be given.
+ */
 window.onload = function() {
   let word_count = count_words(document.body.innerText);
   let reading_time = Math.round(word_count / reading_speed);
