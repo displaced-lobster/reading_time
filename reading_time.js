c75f49a45513f38e8bce02fd8d2b840665c0a528
Richard Mills <scripts-richard@gmail.com>
2018-05-30 13:34:29 -0600

    Added functionality to subtract words in the nav, header, and footer elements.

--- reading_time.js
+++ reading_time.js
@@ -1,5 +1,7 @@
 // Estimated reading speed of 200 words per minute.
-const reading_speed = 200;
+const READING_SPEED = 200;
+
+const DEBUG = false;
 
 
 /*
@@ -31,7 +33,12 @@ function create_element(minutes) {
   let exit = document.createElement("div");
   exit.classList.add("reading_time_element_exit");
 
-  element.innerText = minutes + " min";
+  if (minutes >= 1) {
+    element.innerText = minutes + " min";
+  } else {
+    element.innerText = "<1 min"
+  }
+
   exit.innerText = "x";
 
   element.id = "reading_time_element";
@@ -45,12 +52,41 @@ function create_element(minutes) {
 
 
 /*
- * Only count words and create the element once given the window.onload.
- * Depending on page, can take awhile for window.onload to be given.
+ * Count words for all instances of a specific element and return
+ * the count.
  */
-let word_count = count_words(document.body.innerText);
-let reading_time = Math.round(word_count / reading_speed);
+function count_words_by_element_name(element_name) {
+  let word_count = 0;
+  let elements = document.getElementsByTagName(element_name);
+  DEBUG && console.log(element_name, "count:", elements.length);
 
-if (reading_time > 0) {
-  create_element(reading_time);
+  for (i = 0; i < elements.length; i++) {
+    word_count += count_words(elements[i].innerText);
+  }
+
+  DEBUG && console.log(element_name, "word count:", word_count);
+  return word_count;
 }
+
+
+// Count all words in body of document
+let word_count = count_words(document.body.innerText);
+DEBUG && console.log("Body word count:", word_count);
+
+// Less all words in navigation elements
+word_count -= count_words_by_element_name("nav");
+
+// Less all words in header elements
+word_count -= count_words_by_element_name("header");
+
+// Less all words in footer elements
+word_count -= count_words_by_element_name("footer");
+
+DEBUG && console.log("Word count:", word_count);
+
+// Estimated reading time
+let reading_time = Math.round(word_count / READING_SPEED);
+DEBUG && console.log("Reading time:", reading_time);
+
+// Spawn reading time element
+create_element(reading_time);
